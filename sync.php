bcbfca717b94d38ba74b9c0b05e223cb71ae631b
tpatchg <104855857+tpatchg@users.noreply.github.com>
2025-02-03 13:16:21 -0600

    Update sync.php

--- sync.php
+++ sync.php
@@ -1,21 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require 'config.php';
 
 use Google\Client;
 use Google\Service\Calendar;
-
-// Configuration
-define('CALENDAR_ID', 'ID@group.calendar.google.com');
-define('SERVICE_ACCOUNT_KEY', '/var/www/html/google_service_key.json');
-
-define('WARNING_BELL', 'warningbell.mp3');
-define('START_BELL', 'startbell.mp3');
-define('END_BELL', 'endbell.mp3');
-
-define('PLAY_SCRIPT', '/var/www/html/play.sh');
-define('LOG_FILE', '/var/log/bells/sync.log');
-
-date_default_timezone_set('America/Chicago');
 
 // Initialize Google Client
 function getGoogleClient() {
